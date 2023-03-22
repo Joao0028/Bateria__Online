19b9ebf5d7b386203e7e7fefecb636d3924f3f09
João Paulo Nascimento Silva <120075584+Joao0028@users.noreply.github.com>
2023-03-22 19:45:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+/*
+/*Tecla__Pom*/
+function tocaSomPom (){
+    document.querySelector('#som_tecla_pom').play();    
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+/*Tecla__Clap*/
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+/*Tecla__Tim*/
+function tocaSomTim(){
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+/*Tecla__Puff*/
+function tocaSomPuff(){
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+/*Tecla__Plash*/
+function tocaSomsPlash(){
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash').onclick = tocaSomsPlash;
+
+/*Tecla__Toim*/
+function tocaSomToim(){
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+/*Tecla__Psh*/
+function tocaSomPsh(){
+    document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+/*Tecla__Tic*/
+function tocaSomTic(){
+    document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+/*Tecla__TomS*/
+function tocaSomTom(){
+    document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
+
+
+
+
+
+
+/*querySelector('Nome do codigo') - */
+/*document.querySelector('.tecla_pom') - Serve para chamar o codigo como no CSS que colocamos por exemplo: body{}*/
+/*document.querySelector('.tecla_pom') - muito bom testar antes no controls da ferramenta do desenvolvedor par saber se realme ira chamar a tag, pois pode haver um erro cso esteja escrito errado...*/
+/*document.querySelector('#som_tecla_pom').play() - No DevTools faz com possa escutar o barulho do audio...*/
+
+/*Nuca esquecer de colocar ";" no final dos codigos par não dar erros...*/
